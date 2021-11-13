22ab97e8c6bc7fb09e609e4735b3d8d374f2a0f0
Pratiti28 <79000977+Pratiti28@users.noreply.github.com>
2021-11-13 19:12:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas= createCanvas(550,550);
+    canvas.position(560,150);
+
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on("pose",gotPoses);
+}
+
+function draw(){
+    background("#ae9db8");
+    fill("#967373");
+    stroke("#967373");
+    square(rightwristx,leftwristy,difference);
+    document.getElementById("squareside").innerHTML="side of the square is "+difference+"px";
+}
+
+function modelloaded(){
+    console.log("PoseNet has been initialized");
+}
+
+function gotPoses(results){
+    if(results.length >0){
+        console.log(results);
+        leftx=results[0].pose.leftWrist.x;
+        lefty=results[0].pose.leftWrist.y;
+        leftx=results[0].pose.rightWrist.x;
+        lefty=results[0].pose.rightWrist.y;
+        console.log("rightwristx ="+rightwristx+"leftwristy ="+leftwristy);
+        leftx=results[0].pose.leftWrist.x;
+        rightx=results[0].pose.rightWrist.x;
+        difference=floor(leftx-rightx);
+    }
+
+}
